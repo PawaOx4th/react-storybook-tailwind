e8f3d534ae9279584a0b5de3f39995f0d665b479
pawaox4th <pawaox4th@gmail.com>
2022-01-23 14:43:04 +0700

    refactor: 📦 Add storybook and tailwind and custom for tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+const path = require("path")
+
+module.exports = {
+  content: [
+    path.join(__dirname, "./index.html"),
+    path.join(__dirname, "./src/**/*.{vue,js,ts,jsx,tsx}")
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
